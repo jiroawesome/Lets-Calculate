13ade6c59c06d3c6642fb2ac9534777b9a5438e6
jiro <91537032+jiroawesome@users.noreply.github.com>
2022-02-10 18:14:11 +0800

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,40 @@
+def add(x, y):
+
+   return x + y
+
+def subtract(x, y):
+
+   return x - y
+
+def multiply(x, y):
+
+   return x * y
+
+def divide(x, y):
+
+   return x / y
+
+print("Select operation: ")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Select from above: ")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print("The answer is: ", add(num1,num2))
+
+elif choice == '2':
+   print("The answer is: ", subtract(num1,num2))
+
+elif choice == '3':
+   print("The answer is: ", multiply(num1,num2))
+
+elif choice == '4':
+   print("The answer is: ", divide(num1,num2))
+else:
+   print("Invalid input")

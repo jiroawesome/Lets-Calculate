795a20d3f8f8f0c629f45f649a5ee5b12df76047
jiro <91537032+jiroawesome@users.noreply.github.com>
2022-02-10 18:28:27 +0800

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+
+int calculator(int num1, int num2, int method) {
+	if (method == 1) {
+		return num1 + num2;
+	} else if (method == 2) {
+		return num1 - num2;
+	} else if (method == 3) {
+		return num1 * num2;
+	} else if (method == 4) {
+		return num1 / num2;
+	} else {
+		printf("Invalid input");
+	}
+	
+	return 0;
+}
+
+int main() {
+	int num1, num2, method;
+	printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nSelect from above: ");
+	scanf("%d", &method);
+	
+	printf("Enter the first number: ");
+	scanf("%d", &num1);
+	
+	printf("Enter the second number: ");
+	scanf("%d", &num2);
+	
+	int vals = calculator(num1, num2, method);
+	printf("The result is: %d", vals);
+	
+	return 0;
+}

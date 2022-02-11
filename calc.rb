dd5aca85d291eea83ddce3bcb7025c24a85d9068
Yuan Arib <67492199+Yuan-San@users.noreply.github.com>
2022-02-11 09:10:02 +0000

    changes idk

--- calc.rb
+++ calc.rb
@@ -2,27 +2,44 @@ def get_int_values
   [gets, gets].map{ |s| s.chomp.to_i }
 end
 
-puts "Would you like to [a]dd, [m]ultiply, [d]ivide, or [s]ubtract?"
+puts "Select operations:"
+puts "1. Add"
+puts "2. Subtract"
+puts "3. Multiply"
+puts "4. Divide"
 response = gets.chomp
 
-case response(0).downcase
-when 'add'
-  puts "Which numbers would you like to add?"
-  operator = :+
+case response
+when '1'
+  puts "Which numbers would you like to add? Enter the first number that you would like to Add."
+  n1 = gets.chomp
+  puts "Then, Enter the second number that will be added."
+  n2 = gets.chomp
+  answer = n1.to_f + n2.to_f
+  puts "The answer is: #{n1} + #{n2} = #{answer}"
 
-when 'subtract'
-  puts "Which numbers would you like to subtract?"
-  operator = :-
+when '2'
+  puts "Which numbers would you like to add? Enter the first number that you would like to subtract."
+  n1 = gets.chomp
+  puts "Then, Enter the second number that will subtract the previous number."
+  n2 = gets.chomp
+  answer = n1.to_f - n2.to_f
+  puts "The answer is: #{n1} - #{n2} = #{answer}"
 
-when 'multiply'
-  puts "Which numbers would you like to multiply?"
-  operator = :*
+when '3'
+  puts "Which numbers would you like to add? Enter the first number that you would like to be multiplied."
+  n1 = gets.chomp
+  puts "Then, Enter the second number that will multiply the first number."
+  n2 = gets.chomp
+  answer = n1.to_f * n2.to_f
+  puts "The answer is: #{n1} * #{n2} = #{answer}"
 
-when 'divide'
-  puts "Which numbers would you like to divide?"
-  operator = :/
+when '4'
+  puts "Which numbers would you like to add? Enter the first number that you would like to be divided."
+  n1 = gets.chomp
+  puts "Then, Enter the second number that will divide the first number."
+  n2 = gets.chomp
+  answer = n1.to_f / n2.to_f
+  puts "The answer is: #{n1} / #{n2} = #{answer}"
 
 end
-
-answer = get_int_values.inject(operator)
-puts "The answer is... #{ answer }"
